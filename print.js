f817d80cf2f1db68267e1bb703ea105d53bba683
Nick Lamicela <nupanick@gmail.com>
2022-10-28 15:44:24 -0700

    Let printer breathe a little.

--- print.js
+++ print.js
@@ -1,9 +1,15 @@
-function print(form) {
-    if (form.type == 'list') return `(${form.list.map(print).join(' ')})`;
-    if (form.type == 'string') return `"${form.string}"`;
-    if (form.type == 'number') return form.number.toString();
-    if (form.type == 'symbol') return form.symbol;
-    if (form.type == 'function') return `Function ${form.function.toString()}`;
-    throw Error(`Unknown form: ${JSON.stringify(form)}`);
+const printers = {
+    list: f => `(${f.list.map(print).join(' ')})`,
+    string: f => `"${f.string}"`,
+    number: f => f.number.toString(),
+    symbol: f => f.symbol,
+    function: f => `Function ${f.function.toString()}`,
 }
+
+function print(form) {
+    const printer = printers[form.type];
+    if (!printer) throw Error(`No printer for ${form.type}.`);
+    return printer(form);
+}
+
 module.exports = print;
